c5a29b0769dbae8440faff86fa24acb82c3fa9ec
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2023-01-24 16:41:00 -0500

    completed part 2

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from petfax import create_app
+app = create_app()
+
+""" replaced with code on lines 1-2
 # config                    
 from flask import Flask
 app = Flask(__name__)
@@ -12,4 +16,4 @@ def index():
 @app.route('/pets/')
 def pets(): 
     return 'These are our pets available for adoption!'
-
+"""
