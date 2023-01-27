07a75e743df51fc81c79e889d538b1ad78c279bc
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2023-01-26 19:19:14 -0500

    completed part 7

--- petfax/pet.py
+++ petfax/pet.py
@@ -5,15 +5,15 @@ pets = json.load(open('pets.json'))
 # print(pets)
 
 
-bp = Blueprint('pet', __name__, url_prefix="/pets")
+bp = Blueprint('pet', __name__, url_prefix = '/pets')
 
 
 @bp.route('/')
 def index():
-    return render_template('pets/index.html', pets=pets)
+    return render_template('pets/index.html', pets = pets)
 
 
 @bp.route('/<int:id>')
 def show(id):
     pet = pets[id - 1]
-    return render_template('pets/show.html', pet=pet)
+    return render_template('pets/show.html', pet = pet)
